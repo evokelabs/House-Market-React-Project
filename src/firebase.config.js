406f8ad88692492e033200ec7c06502b6f444787
Adrian Pikios <adrian@evokelabs.com>
2022-05-05 20:17:55 +1000

    init commit

---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAEHCAt-kHRd5hNDwrEpdfHP-lcABZeBpk',
+  authDomain: 'house-marketplace-app-ab969.firebaseapp.com',
+  projectId: 'house-marketplace-app-ab969',
+  storageBucket: 'house-marketplace-app-ab969.appspot.com',
+  messagingSenderId: '460024894259',
+  appId: '1:460024894259:web:9820abd8dd9597edfde446',
+}
+
+export const db = getFirestore()
