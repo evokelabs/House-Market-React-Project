aebaa7d9b62979125b2af87fe30564fe1cd82283
Adrian Pikios <adrian@evokelabs.com>
2022-05-05 21:05:45 +1000

    Navigational Skelly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-out' element={<SignUp />} />
+        </Routes>
+        <NavBar />
+      </Router>
     </>
   )
 }
